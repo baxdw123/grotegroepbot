322a1737e6a3dfece5fd770b078064ea5ccd00be
baxdw123 <63054037+baxdw123@users.noreply.github.com>
2020-08-25 16:47:07 +0200

    init
    init

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ client.user.setActivity("Minecraft", {type: "PLAYING"});
 
 
 client.on("message", async message => {
+
  
     if(message.author.bot) return;
  
@@ -35,8 +36,17 @@ client.on("message", async message => {
 
 if(message.content == '!roll')
 var roll =(Math.floor(Math.random()*200)+1);
-   if (roll = 1 )
-    {message.reply('Wowza!');
+   if (roll = 10 )
+   {message.reply('10'),
+   message.reply('9')
+   message.reply('8')
+   message.reply('7')
+   message.reply('6')
+   message.reply('5')
+   message.reply('4')
+   message.reply('3')
+   message.reply('2')
+   message.reply('1')
 }
         else {message.reply('lol!');
     }
