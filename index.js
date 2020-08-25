a9a14c59aced6f69351f91eb39b4e9188b6476de
baxdw123 <63054037+baxdw123@users.noreply.github.com>
2020-08-25 16:38:29 +0200

    init
    init

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ client.user.setActivity("Minecraft", {type: "PLAYING"});
 
 
 });
+
+
+
+client.on("message", async message => {
+ 
+    if(message.author.bot) return;
+ 
+    if(message.channel.type === "dm") return;
+ 
+    var prefix = botConfig.prefix;
+ 
+    var messageArray = message.content.split(" ");
+ 
+    var command = messageArray[0];
+ 
+    if (command === `${prefix}hallo`) {
+ 
+        return message.channel.send("Hallo!!");
+    
+
+    }
+
+if(message.content == '!roll')
+var roll =(Math.floor(Math.random()*200)+1);
+   if (roll = 1 )
+    {message.reply('Wowza!');
+}
+        else {message.reply('lol!');
+    }
+});
